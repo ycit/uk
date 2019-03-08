d2f7e6850e953073831b281a7d8fafec995c6708
xlch <ycitcxl@163.com>
2019-03-08 16:39:50 +0800

    接口统一返回定义

---
+++ common/src/main/java/com/ycit/common/bean/resp/ResponseMsg.java
@@ -0,0 +1,40 @@
+package com.ycit.common.bean.resp;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 接口统一返回格式定义
+ *
+ * @author keda
+ * @date 2019/3/7
+ */
+@Data
+public class ResponseMsg<T> {
+
+    private List<T> results;
+    private int total;
+    private int code;
+    private String msg;
+    private long timestamp;
+
+    public static <T> ResponseMsg<T> ok(List<T> results, int total) {
+        ResponseMsg<T> responseMsg = new ResponseMsg<T>();
+        responseMsg.setResults(results);
+        responseMsg.setCode(200);
+        responseMsg.setTimestamp(System.currentTimeMillis());
+        return responseMsg;
+    }
+
+    public static ResponseMsg error(int code, String msg) {
+        ResponseMsg responseMsg = new ResponseMsg();
+        responseMsg.setCode(code);
+        responseMsg.setMsg(msg);
+        responseMsg.setTimestamp(System.currentTimeMillis());
+        return responseMsg;
+    }
+
+
+
+}
